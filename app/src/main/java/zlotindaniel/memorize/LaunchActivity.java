73f7027df00559faf00a522ccf5ed41fb9939d85
Daniel Zlotin <danielzl@wix.com>
2017-10-17 11:52:59 +0300

    locked db

--- app/src/main/java/zlotindaniel/memorize/LaunchActivity.java
+++ app/src/main/java/zlotindaniel/memorize/LaunchActivity.java
@@ -7,10 +7,37 @@ import android.support.annotation.Nullable;
 import zlotindaniel.memorize.android.topics.TopicsActivity;
 
 public class LaunchActivity extends BaseActivity {
+
 	@Override
 	protected void onCreate(@Nullable final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		startActivity(new Intent(this, TopicsActivity.class));
 		finish();
 	}
+
+//	private void login() {
+//		FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+//		Toast.makeText(this, MoreObjects.firstNonNull(currentUser, "null").toString(), Toast.LENGTH_SHORT).show();
+//
+//		AuthUI.IdpConfig googleProvider = new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build();
+//		Intent intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Lists.newArrayList(googleProvider)).build();
+//		startActivityForResult(intent, REQUEST_CODE);
+//	}
+
+//	@Override
+//	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+//		super.onActivityResult(requestCode, resultCode, data);
+//		if (requestCode != REQUEST_CODE) return;
+//
+//		IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+//
+//		if (resultCode != RESULT_OK || idpResponse == null) {
+//			Toast.makeText(this, "sign in error", Toast.LENGTH_SHORT).show();
+//			return;
+//		}
+//
+//		Toast.makeText(this, "success! " + idpResponse.getProviderType(), Toast.LENGTH_SHORT).show();
+//		Toast.makeText(this, idpResponse.getProviderType(), Toast.LENGTH_SHORT).show();
+//		Toast.makeText(this, idpResponse.getEmail(), Toast.LENGTH_SHORT).show();
+//	}
 }
