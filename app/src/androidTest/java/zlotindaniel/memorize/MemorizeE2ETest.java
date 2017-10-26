c4461287d13695fcfbc438f935d53ce7a700a087
Daniel Zlotin <danielzl@wix.com>
2017-10-27 00:48:13 +0300

    fix e2e

--- app/src/androidTest/java/zlotindaniel/memorize/MemorizeE2ETest.java
+++ app/src/androidTest/java/zlotindaniel/memorize/MemorizeE2ETest.java
@@ -5,10 +5,6 @@ import org.junit.Test;
 import zlotindaniel.memorize.android.cards.CardsView;
 import zlotindaniel.memorize.android.topics.TopicsView;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-
 public class MemorizeE2ETest extends BaseE2ETest {
 
 	@Test
@@ -39,18 +35,5 @@ public class MemorizeE2ETest extends BaseE2ETest {
 		assertDisplayed("Card Answer 3");
 		clickOn("Card Answer 3");
 		assertDisplayed("Card Question 1");
-	}
-
-	@Test
-	public void accessAdminMode() throws Exception {
-		launchApp();
-		waitForText(TopicsView.TITLE);
-		waitForText("Topic Name 1");
-
-		onView(withContentDescription("More options")).perform(click());
-		assertDisplayed(TopicsView.ADMIN_MODE_TITLE);
-		clickOn(TopicsView.ADMIN_MODE_TITLE);
-
-
 	}
 }
